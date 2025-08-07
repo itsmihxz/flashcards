093b21c519a687c656e3c41cb65ac1d2a619ba71
itsmihxz <mirella.fraga.nascimento@escola.pr.gov.br>
2025-08-07 12:17:07 -0300

    Add files via upload

---
+++ flashcards-main/Mirella/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Geografia',
+    'Quais são os três modelos de produção mais conhecidos',
+    'Taylorismo, Fordismo e Toyotismo'
+)
+
+criaCartao(
+    'Biologia',
+    'A partir de qual classe do Reino Plantae aparecem vasos condutores?',
+    'A partir das Pteridófitas'
+)
+
+criaCartao(
+    'Biologia',
+    'O que são plantas criptógamas?',
+    'São plantas que tem o sistema produtor pouco visível, isto é, não produz sementes'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual a sequência evolutiva das plantas?',
+    'Briófitas, Pteridófitas, Gimnospermas e Angiospermas'
+)
+
+criaCartao(
+    'Política Global',
+    'Qual a nacionalidade do novo papa?',
+    'O novo papa é Americano'
+)
+
+criaCartao(
+    'Política Global',
+    'O que é um impeachment?',
+    'É um processo político aplicado com o objetivo de retirar alguém do cargo'
+)
