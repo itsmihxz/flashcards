64790cb061c05f6affd9043bd4dec921425e52af
itsmihxz <mirella.fraga.nascimento@escola.pr.gov.br>
2025-08-12 14:41:40 -0300

    Add files via upload

---
+++ flashcards-main/flashcards-main/Mirella/perguntas.js
@@ -0,0 +1,37 @@
+
+
+criaCartao(
+    'Biologia',
+    'A partir de qual classe do Reino Plantae aparecem vasos condutores?',
+    'A partir das Pteridófitas'
+)
+
+criaCartao(
+    'Biologia',
+    'O que são plantas criptógamas?',
+    'São plantas que tem o sistema produtor pouco visível, isto é, não produzem sementes'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual a sequência evolutiva das plantas?',
+    'Briófitas, Pteridófitas, Gimnospermas e Angiospermas'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são os três modelos de produção mais conhecidos?',
+    'Taylorismo, Fordismo e Toyotismo'
+)
+
+criaCartao(
+    'Política Global',
+    'O que é um conclave?',
+    'Um processo no qual cardeais se reúnem para eleger um novo papa'
+)
+
+criaCartao(
+    'Política Global',
+    'O que é um impeachment?',
+    'É um processo político aplicado com o objetivo de retirar alguém do cargo'
+)
